6a0592e6c97c52d5ea66cd842ce2fe29fcfe9aba
Samantha Brennan <samanthabrennan@usf.edu>
2025-09-21 18:24:40 -0400

    completed step 2 - created an array of 5 products

--- cc_4.js
+++ cc_4.js
@@ -1,4 +1,14 @@
 //Coding Challenge 4
 
-test
+//Step 2: Create an array of objects for 5 products
+
+let products = [
+    {productName: "headphones", productCategory: "electronics", productPrice: "149.99", productInventoryCount: "32"},
+    {productName: "tube socks", productCategory: "apparel", productPrice: "14.85", productInventoryCount: "56"},
+    {productName: "ski mask", productCategory: "apparel", productPrice: "9.97", productInventoryCount: "11"},
+    {productName: "tortellini", productCategory: "groceries", productPrice: "6.59", productInventoryCount: "18"},
+    {productName: "paper towels", productCategory: "household", productPrice: "11.49", productInventoryCount: "27"},    
+]
+
+//products.forEach(products => console.log(`Name: ${products.productName}, Category: ${products.productCategory}, Price: $${products.productPrice}, Inventory Count: ${products.productInventoryCount}`))
 
