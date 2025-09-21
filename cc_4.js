471086d84fc8778315e3490df683c15cce8c9722
Samantha Brennan <samanthabrennan@usf.edu>
2025-09-21 18:31:38 -0400

    Updated 1 product name, price, category

--- cc_4.js
+++ cc_4.js
@@ -5,10 +5,12 @@
 let products = [
     {productName: "headphones", productCategory: "electronics", productPrice: "149.99", productInventoryCount: "32"},
     {productName: "tube socks", productCategory: "apparel", productPrice: "14.85", productInventoryCount: "56"},
-    {productName: "ski mask", productCategory: "apparel", productPrice: "9.97", productInventoryCount: "11"},
+    {productName: "standing desk", productCategory: "furniture", productPrice: "499.97", productInventoryCount: "11"},
     {productName: "tortellini", productCategory: "groceries", productPrice: "6.59", productInventoryCount: "18"},
     {productName: "paper towels", productCategory: "household", productPrice: "11.49", productInventoryCount: "27"},    
 ]
 
 //products.forEach(products => console.log(`Name: ${products.productName}, Category: ${products.productCategory}, Price: $${products.productPrice}, Inventory Count: ${products.productInventoryCount}`))
+
+//Step 3: write for...of loop to add discounts & switch statement
 
