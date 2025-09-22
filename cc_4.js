d4f0c4f5909a817293250a3dad186603f018f13e
Samantha Brennan <samanthabrennan@usf.edu>
2025-09-21 21:38:33 -0400

    completed step 5 - created cusstomerloop and calculated discounted total and reduced inventory

--- cc_4.js
+++ cc_4.js
@@ -27,12 +27,11 @@ for (const product of products) {
             case "household": //group cases with same discount rate
             categoryDiscount = 0.10; //10% discount
             break;  
-        default:
+            default:
             categoryDiscount = 0; //no discount
             break;
     }
-    let discountPrice = product.productPrice * (1 - categoryDiscount);
-   
+      
 //   console.log(`Product: ${product.productName}, Category: ${product.productCategory}`, `Original Price: $${product.productPrice}, 
 //     Discount: ${(categoryDiscount * 100).toFixed(0)}%,`, `Discounted Price: $${discountPrice.toFixed(2)}`)
  }
@@ -40,7 +39,7 @@ for (const product of products) {
 //Step 4 - create variable customer types and use if/else if to apply additional discounts.  
 
 let customerType = "senior";
-console.log(customerType)
+//console.log(customerType)
 let extraDiscount = 0;
 
 if (customerType === "student") {
@@ -50,5 +49,40 @@ if (customerType === "student") {
 } else {
     extraDiscount = 0; // default is 0 discount
 }
+//console.log(`Customer Type: ${customerType}, Extra Discount: ${(extraDiscount * 100).toFixed(0)}%`);
 
-console.log(`Customer Type: ${customerType}, Extra Discount: ${(extraDiscount * 100).toFixed(0)}%`);
+// step 5 - use for-loops to simulate checkout process
+for (let i = 1; i <= 3; i++) {
+    let subtotal = 0;
+
+    for (const product of products) {
+    let categoryDiscount = 0; // 0% discount is the default
+    switch (product.productCategory) {
+        case "electronics":
+            categoryDiscount = 0.20; //20% discount
+            break;
+            case "apparel": 
+            categoryDiscount = 0.15; //15% discount
+            break;
+            case "groceries":
+            case "household": //group cases with same discount rate
+            categoryDiscount = 0.10; //10% discount
+            break;  
+        default:
+            categoryDiscount = 0; //no discount
+            break;
+    }
+let priceAfterCategory = (+product.productPrice) * (1 - categoryDiscount);
+subtotal = subtotal + priceAfterCategory;
+//console.log(916subtotal.toFixed(2));
+let inventory = +product.productInventoryCount;
+if (inventory > 0) {
+    product.productInventoryCount --;
+    //console.log(product.productName + "inventory now: " + product.productInventoryCount);
+
+}
+    }
+    const finalTotal = subtotal * (1 - extraDiscount);
+    console.log("Customer " + i + " Total: $" + finalTotal.toFixed(2)); 
+    console.log("==============================================================")
+}
