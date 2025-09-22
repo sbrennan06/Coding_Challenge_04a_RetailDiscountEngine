16ab7af1445667d1b9f5a94969c5aad1ce7ab3e7
Samantha Brennan <samanthabrennan@usf.edu>
2025-09-21 20:07:41 -0400

    completed step 4 - if/else scenarios for extra discount

--- cc_4.js
+++ cc_4.js
@@ -33,11 +33,22 @@ for (const product of products) {
     }
     let discountPrice = product.productPrice * (1 - categoryDiscount);
    
-  console.log(`Product: ${product.productName}, Category: ${product.productCategory}`, `Original Price: $${product.productPrice}, 
-    Discount: ${(categoryDiscount * 100).toFixed(0)}%,`, `Discounted Price: $${discountPrice.toFixed(2)}`)
+//   console.log(`Product: ${product.productName}, Category: ${product.productCategory}`, `Original Price: $${product.productPrice}, 
+//     Discount: ${(categoryDiscount * 100).toFixed(0)}%,`, `Discounted Price: $${discountPrice.toFixed(2)}`)
  }
 
+//Step 4 - create variable customer types and use if/else if to apply additional discounts.  
 
+let customerType = "senior";
+console.log(customerType)
+let extraDiscount = 0;
 
+if (customerType === "student") {
+    extraDiscount = 0.05; // 5% extra discount
+} else if (customerType === "senior") {
+    extraDiscount = 0.07; // 7% extra discount 
+} else {
+    extraDiscount = 0; // default is 0 discount
+}
 
-
+console.log(`Customer Type: ${customerType}, Extra Discount: ${(extraDiscount * 100).toFixed(0)}%`);
